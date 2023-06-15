df2d660617502728a3dccf990660d86b700cab09
Chakresh2001 <jha.chakresh2001@gmail.com>
2023-06-15 22:55:46 +0530

    check:

---
+++ src/Redux/AuthReducer/reducer.js
@@ -0,0 +1,39 @@
+import { GET_AUTH_FAILURE, GET_AUTH_REQUEST, GET_AUTH_SUCCESS } from "../actionType"
+
+const initialState = {
+    loading : false,
+    isAuth: false,
+    error : false
+}
+
+export const reducer = (state=initialState,{type})=>{
+    switch(type){
+        case GET_AUTH_REQUEST : {
+            return {
+                ...state,
+                loading : true,
+                isAuth: false,
+                error : false
+            }
+        }
+        case GET_AUTH_SUCCESS : {
+            return {
+                ...state,
+                loading : false,
+                isAuth: true,
+                error : false
+            }
+        }
+        case GET_AUTH_FAILURE : {
+            return {
+                ...state,
+                loading : false,
+                isAuth: false,
+                error : true
+            }
+        }
+
+
+        default : return {state}
+    }
+}
