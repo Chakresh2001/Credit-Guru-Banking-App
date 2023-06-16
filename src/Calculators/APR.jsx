8d71735e73a995994e43729705daf299a6f746fe
Shreekant-totla <shreekanttotla3@gmail.com>
2023-06-16 16:36:26 +0530

    day3_fp08_171

--- src/Calculators/APR.jsx
+++
@@ -1,9 +0,0 @@
-const emi = 25000;
-const interestRate = 7;
-const loanTenure = 5;
-const principalLoanAmount = 1262550;
-
-
-
-const apr = calculateApr(principalLoanAmount, emi, loanTenure);
-console.log("Annual Percentage Rate (APR):", apr.toFixed(2));
